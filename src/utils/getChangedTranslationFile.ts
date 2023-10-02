a42eade946e828e56a010e2576dc2147f9523b54
Favot <70208301+Favot@users.noreply.github.com>
2023-10-02 08:40:36 +0200

    Update getChangedTranslationFile command to get the rigth default file

--- src/utils/getChangedTranslationFile.ts
+++ src/utils/getChangedTranslationFile.ts
@@ -2,16 +2,18 @@ import { executeCommand } from "./executeCommand";
 
 export const getChangedTranslationFile = ({
   defaultLanguage,
-  pathToDefaultLanguage,
 }: {
   defaultLanguage: string;
-  pathToDefaultLanguage: string;
 }): Promise<string | Error> => {
-  const command = `git diff-index --name-only --cached --diff-filter=d HEAD | grep -E "^${pathToDefaultLanguage}.${defaultLanguage}.json$"`;
+  const command = `git diff-index --name-only --cached --diff-filter=d HEAD | grep '${defaultLanguage}.json'`;
   const output = executeCommand(command);
 
   if (!output) {
-    return Promise.reject(new Error("No translation file updates found"));
+    return Promise.reject(
+      new Error(
+        "No translation file updates found, make sure you correctly staged the file"
+      )
+    );
   }
 
   const isMultipleFiles = output.split("\n").length > 1;
