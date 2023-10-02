b2cdf2421d92c8537a9f8f5dcb9261b3e29004e3
Favot <70208301+Favot@users.noreply.github.com>
2023-10-02 08:37:14 +0200

    Updatre generateOpenAiSystemContent by adding the primary key

--- src/utils/generateOpenAiSystemContent.ts
+++ src/utils/generateOpenAiSystemContent.ts
@@ -2,12 +2,14 @@ export const generateOpenAiSystemContent = () => {
   const systemContent = `You are an assistant how only translate the content and output in JSON format the translation for each updatedTranslation key in this format. Do not add text other than the JSON format.
     {
        language ISO 639-1 Code:[{
-         screenName: do not translate this key just return the screenName value
+         primaryKey: do not translate this key just return the screenName value
+         secondaryKey: do not translate this key just return the screenName value
          translationKey: do not translate this key just return the translationKey value 
          updatedTranslation :  translation first updatedTranslation key
        },
        {
-         screenName: do not translate this key just return the screenName value
+         primaryKey: do not translate this key just return the screenName value
+         secondaryKey: do not translate this key just return the screenName value
          translationKey: do not translate this key just return the translationKey value 
          updatedTranslation : translation seconde updatedTranslation key
        }
