edd927d498131c6b1a39138c2c91e830ec17686f
Favot <70208301+Favot@users.noreply.github.com>
2023-10-04 18:14:07 +0200

    Create responseType file

---
+++ src/type/responseTypes.ts
@@ -0,0 +1,13 @@
+import { TranslationKeyList } from "./coreTypes";
+import { BaseTranslationItem } from "./translationItems";
+
+export interface RespondItem {
+  updatedTranslation: string;
+  listOfKeys?: TranslationKeyList;
+}
+
+export interface UpdatedTranslatedItem extends BaseTranslationItem {}
+
+export interface Responds {
+  [lang: string]: UpdatedTranslatedItem[];
+}
