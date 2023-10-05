00b9ecd0f940780ffdbfc99b94c63a7d9a0ccdb9
Favot <70208301+Favot@users.noreply.github.com>
2023-10-05 09:46:26 +0200

    Fix eslint issue by removing type any in the code

--- src/utils/updateOtherLanguage.ts
+++ src/utils/updateOtherLanguage.ts
@@ -1,54 +1,67 @@
-import fs from 'fs';
-import path from 'path';
-import { Responds, UpdatedTranslatedItem } from '../type';
-
+import fs from 'fs'
+import path from 'path'
+import { NestedObject, Responds, UpdatedTranslatedItem } from '../type'
 export const updateOtherLanguage = ({
   otherLanguage,
   responds,
   translationDirectory,
 }: {
-  otherLanguage: string[];
-  responds: Responds;
-  translationDirectory: string;
+  otherLanguage: string[]
+  responds: Responds
+  translationDirectory: string
 }) => {
-  otherLanguage.forEach((lang) => {
-    console.log(`Updating ${lang} translation file...`);
+  function isNestedObject(value: unknown): value is NestedObject {
+    return typeof value === 'object' && value !== null && !Array.isArray(value)
+  }
 
-    const filePath = path.join(translationDirectory, `${lang}.json`);
+  otherLanguage.forEach((lang) => {
+    console.log(`Updating ${lang} translation file...`)
+
+    const filePath = path.join(translationDirectory, `${lang}.json`)
 
     const existingTranslations = fs.existsSync(filePath)
       ? JSON.parse(fs.readFileSync(filePath, 'utf-8'))
-      : {};
+      : {}
 
     responds[lang].forEach((item: UpdatedTranslatedItem) => {
-      const { updatedTranslation, listOfKeys } = item;
+      const { updatedTranslation, listOfKeys } = item
 
-      function updateOrCreate(obj: any, keys: string[], index: number) {
-        const key = keys[index];
+      function updateOrCreateKey(
+        obj: NestedObject,
+        keys: string[],
+        index: number,
+      ) {
+        const key = keys[index]
 
-        if (key === 'context') return;
+        if (key === 'context' || key === 'appContext') return
 
         if (index === keys.length - 1) {
           console.log(
-            `In the ${lang} file, updating key: "${key}" wit the text : ${updatedTranslation}`,
-          );
-          obj[key] = updatedTranslation;
+            `In the ${lang} file, updating key: "${key}" with the text: ${updatedTranslation}`,
+          )
+          obj[key] = updatedTranslation
         } else {
           if (!obj[key] || typeof obj[key] !== 'object') {
-            console.log(`In the ${lang} file, creating new key ${key}`);
-            obj[key] = {};
+            console.log(`In the ${lang} file, creating new key ${key}`)
+            obj[key] = {}
           }
-          updateOrCreate(obj[key], keys, index + 1);
+          if (isNestedObject(obj[key])) {
+            updateOrCreateKey(obj[key] as NestedObject, keys, index + 1)
+          } else {
+            console.error(
+              `Expected a NestedObject but got a ${typeof obj[key]}`,
+            )
+          }
         }
       }
 
-      updateOrCreate(existingTranslations, listOfKeys, 0);
-    });
+      updateOrCreateKey(existingTranslations, listOfKeys, 0)
+    })
 
     fs.writeFileSync(
       filePath,
       JSON.stringify(existingTranslations, null, 2),
       'utf-8',
-    );
-  });
-};
+    )
+  })
+}
