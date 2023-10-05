6e8532faf774a34d1cd86ab82e6cb6f02a316411
Favot <70208301+Favot@users.noreply.github.com>
2023-10-05 22:07:43 +0200

    Update the import in the test/index file

--- __test__/index.ts
+++ __test__/index.ts
@@ -1,44 +1,32 @@
-import env from 'dotenv'
 import { executeCommand } from '../src/generalUtils'
 import { updateTranslationFileOnCommit } from '../src/index'
-import { baseDirectory, homeScreenDirectory } from './type'
+import { defaultLanguage, openAiApiKey, otherLanguage } from './config'
 import {
   clearAllOtherTranslationFilesWithEmptyFile,
-  clearEnGbFiles,
-  copyTemplateWithContextToEnGb,
-  copyTemplateWithTextToEnGB,
-  stageEnGbFiles,
+  clearDefaultFiles,
+  copyTemplateWithContextToDefaultLanguage,
+  copyTemplateWithTextToDefaultLanguageFile,
+  stageDefaultLanguageFiles,
 } from './utils'
-const openAiApiKey = env.config().parsed?.OPENAI_API_KEY
 
-const otherLanguage = ['en-US', 'de-DE', 'fr-FR', 'zh-CN']
+clearDefaultFiles({ defaultLanguage })
 
-clearEnGbFiles()
+copyTemplateWithContextToDefaultLanguage({ defaultLanguage })
 
-copyTemplateWithContextToEnGb()
-
-// // clear all the other translation files by emptyTranslationModel to de-DE.json, fr-FR.json, zh-CN.json and en-US.json
-// console.log('clear all the other translation files by emptyTranslationModel')
 clearAllOtherTranslationFilesWithEmptyFile({
   translationFiles: otherLanguage,
 })
 
-// commit the changes
 console.log('commit the changes')
-executeCommand(`git add ${baseDirectory}/en-GB.json`)
-executeCommand(`git add ${homeScreenDirectory}/en-GB.json`)
+
+stageDefaultLanguageFiles({ defaultLanguage })
 
 executeCommand(`git commit -m "test: update en-GB.json"`)
 
-// copy the exampleTranslation.json to en-GB.json
+copyTemplateWithTextToDefaultLanguageFile({ defaultLanguage })
 
-copyTemplateWithTextToEnGB()
+stageDefaultLanguageFiles({ defaultLanguage })
 
-// stage the change
-
-stageEnGbFiles()
-
-// run the script
 console.log('run the script')
 updateTranslationFileOnCommit({
   openAiApiKey,
