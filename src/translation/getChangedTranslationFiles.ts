9c89acfea84e46996bf9a25ac2e5610a03b29a56
Favot <70208301+Favot@users.noreply.github.com>
2023-10-05 22:09:50 +0200

    Update the function getChangedTranslationFiles
    - update the function to return a array of updated files
    - Update test according to the new scope

---
+++ src/translation/getChangedTranslationFiles.ts
@@ -0,0 +1,26 @@
+import { executeCommand } from '../generalUtils/executeCommand'
+
+export const noTranslationUpdatesFound =
+  'No base translation file updates found, skipping'
+
+export const getChangedTranslationFiles = ({
+  defaultLanguage,
+}: {
+  defaultLanguage: string
+}): Promise<string[] | string> => {
+  const command = `git diff-index --name-only --cached --diff-filter=d HEAD | grep '${defaultLanguage}.json'`
+  const output = executeCommand(command)
+
+  if (!output) {
+    return Promise.resolve(noTranslationUpdatesFound)
+  }
+
+  const isMultipleFiles = output.includes('\n')
+
+  if (isMultipleFiles) {
+    const outputArray = output.split('\n')
+    return Promise.resolve(outputArray)
+  }
+
+  return Promise.resolve(output)
+}
