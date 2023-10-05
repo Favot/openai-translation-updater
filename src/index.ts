40378e94a780a566371da9703184c676f2267137
Favot <70208301+Favot@users.noreply.github.com>
2023-10-05 22:10:59 +0200

    Update the function updateTranslationFileOnCommit to handle mutilple updated files

--- src/index.ts
+++ src/index.ts
@@ -1,140 +1,143 @@
-import OpenAI from 'openai';
+import OpenAI from 'openai'
 
 import {
   addListOfKeysToRespondContent,
   generateOpenAiAssistantContent,
   generateOpenAiSystemContent,
+  getCurrentDirectory,
   getIsRespondsValid,
-} from './generalUtils';
+} from './generalUtils'
 import {
-  getChangedTranslationFile,
-  noTranslationUpdatesFound,
+  getChangedTranslationFiles,
   processTranslatedFile,
   updateOtherLanguage,
-} from './translation';
-import { Responds } from './type';
+} from './translation'
+import { Responds } from './type'
 
 export const updateTranslationFileOnCommit = async ({
   openAiApiKey,
-  translationDirectory,
   defaultLanguage,
   otherLanguage,
 }: {
-  openAiApiKey: string | undefined;
-  translationDirectory: string;
-  defaultLanguage: string;
-  otherLanguage: string[];
+  openAiApiKey: string | undefined
+  defaultLanguage: string
+  otherLanguage: string[]
 }) => {
   try {
     if (!openAiApiKey) {
-      console.log('OpenAI API key is required');
-      return;
+      console.log('OpenAI API key is required')
+      return
     }
-    const openai = new OpenAI({
-      apiKey: openAiApiKey,
-    });
 
-    console.log('get changed translation file');
+    console.log('get changed translation file')
 
-    const changedTranslationFile = await getChangedTranslationFile({
+    const changedTranslationFiles = await getChangedTranslationFiles({
       defaultLanguage,
-    });
+    })
 
-    if (changedTranslationFile === noTranslationUpdatesFound) {
-      return;
+    if (typeof changedTranslationFiles === 'string') {
+      console.log(changedTranslationFiles)
+      return
     }
 
-    if (changedTranslationFile instanceof Error) {
-      console.log(changedTranslationFile.message);
-      return;
+    if (changedTranslationFiles instanceof Error) {
+      console.log(changedTranslationFiles.message)
+      return
     }
 
-    console.log('process translated file');
-
-    const updatedTranslationData = processTranslatedFile(
-      changedTranslationFile,
-    );
-
-    if (
-      !updatedTranslationData ||
-      !updatedTranslationData?.updatedItems ||
-      updatedTranslationData?.updatedItems.length === 0
-    ) {
-      console.log(`No translation file updates found`);
-      return;
-    }
-
-    console.log('generate open ai assistant content');
-
-    console.log('generate open ai system content');
-
-    const appContext = updatedTranslationData.appContext;
-
-    updatedTranslationData.updatedItems.forEach(async (updatedItem) => {
+    changedTranslationFiles.forEach((file) => {
+      const currentTranslationDirectory = getCurrentDirectory(file)
       console.log(
-        `Request open ai to translate the text: "${updatedItem.updatedTranslation}"`,
-      );
+        '🚀 ~ file: index.ts:55 ~ changedTranslationFiles.forEach ~ currentTranslationDirectory:',
+        currentTranslationDirectory,
+      )
 
-      const assistantContent = generateOpenAiAssistantContent({
-        updatedTranslationData: updatedItem,
-        appContext,
-        languagesList: otherLanguage,
-      });
+      console.log('process translated file')
 
-      const systemContent = generateOpenAiSystemContent();
+      const updatedTranslationData = processTranslatedFile(file)
 
-      const chatCompletion = await openai.chat.completions.create({
-        messages: [
-          {
-            role: 'system',
-            content: systemContent,
-          },
-          {
-            role: 'assistant',
-            content: assistantContent,
-          },
-        ],
-        model: 'gpt-4',
-      });
-
-      console.log('update response from open ai');
-      const respondContent = chatCompletion.choices[0].message.content;
-
-      if (!respondContent) {
-        console.log('OpenAI dit not return the expected content');
-        return;
+      if (
+        !updatedTranslationData ||
+        !updatedTranslationData?.updatedItems ||
+        updatedTranslationData?.updatedItems.length === 0
+      ) {
+        console.log(`No translation file updates found`)
+        return
       }
 
-      console.log('Parse respond content');
-      const responds: Responds = JSON.parse(
-        Buffer.from(respondContent, 'utf-8').toString('utf-8'),
-      );
+      console.log('generate open ai assistant content')
 
-      const isRespondsValid = getIsRespondsValid(responds, otherLanguage);
+      console.log('generate open ai system content')
 
-      if (!isRespondsValid) {
+      const appContext = updatedTranslationData.appContext
+
+      updatedTranslationData.updatedItems.forEach(async (updatedItem) => {
         console.log(
-          'OpenAI did not return the expected content, the content returned was :',
-          responds,
-        );
+          `Request open ai to translate the text: "${updatedItem.updatedTranslation}"`,
+        )
 
-        return;
-      }
+        const assistantContent = generateOpenAiAssistantContent({
+          updatedTranslationData: updatedItem,
+          appContext,
+          languagesList: otherLanguage,
+        })
 
-      const updatedRespondContentWithListOfKeys = addListOfKeysToRespondContent(
-        updatedItem.listOfKeys,
-        responds,
-      );
+        const systemContent = generateOpenAiSystemContent()
+        const openai = new OpenAI({
+          apiKey: openAiApiKey,
+        })
 
-      console.log('update other language');
-      updateOtherLanguage({
-        otherLanguage,
-        responds: updatedRespondContentWithListOfKeys,
-        translationDirectory: translationDirectory,
-      });
-    });
+        const chatCompletion = await openai.chat.completions.create({
+          messages: [
+            {
+              role: 'system',
+              content: systemContent,
+            },
+            {
+              role: 'assistant',
+              content: assistantContent,
+            },
+          ],
+          model: 'gpt-4',
+        })
+
+        console.log('update response from open ai')
+        const respondContent = chatCompletion.choices[0].message.content
+
+        if (!respondContent) {
+          console.log('OpenAI dit not return the expected content')
+          return
+        }
+
+        console.log('Parse respond content')
+        const responds: Responds = JSON.parse(
+          Buffer.from(respondContent, 'utf-8').toString('utf-8'),
+        )
+
+        const isRespondsValid = getIsRespondsValid(responds, otherLanguage)
+
+        if (!isRespondsValid) {
+          console.log(
+            'OpenAI did not return the expected content, the content returned was :',
+            responds,
+          )
+
+          return
+        }
+
+        const updatedRespondContentWithListOfKeys =
+          addListOfKeysToRespondContent(updatedItem.listOfKeys, responds)
+
+        console.log('update other language')
+        updateOtherLanguage({
+          otherLanguage,
+          responds: updatedRespondContentWithListOfKeys,
+          translationDirectory: currentTranslationDirectory,
+        })
+      })
+    })
   } catch (error) {
-    console.log(error);
-    return;
+    console.log(error)
+    return
   }
-};
+}
