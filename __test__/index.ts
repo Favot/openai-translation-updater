fc79cac4d4a5a42110f04f268e2a4bb888ba2728
Favot <70208301+Favot@users.noreply.github.com>
2023-10-05 21:47:39 +0200

    Rework the main test function with the new function created

--- __test__/index.ts
+++ __test__/index.ts
@@ -1,57 +1,47 @@
 import env from 'dotenv'
 import { executeCommand } from '../src/generalUtils'
 import { updateTranslationFileOnCommit } from '../src/index'
-
+import { baseDirectory, homeScreenDirectory } from './type'
+import {
+  clearAllOtherTranslationFilesWithEmptyFile,
+  clearEnGbFiles,
+  copyTemplateWithContextToEnGb,
+  copyTemplateWithTextToEnGB,
+  stageEnGbFiles,
+} from './utils'
 const openAiApiKey = env.config().parsed?.OPENAI_API_KEY
-const translationDirectory = './__test__/localization/translations'
+
 const otherLanguage = ['en-US', 'de-DE', 'fr-FR', 'zh-CN']
 
-// Remove en-GB.json from translations folder
-console.log('Remove en-GB.json from translations folder')
-executeCommand(`rm ${translationDirectory}/en-GB.json`)
+clearEnGbFiles()
 
-// copy the emptyTranslationModel.json to en-GB.json
-console.log('copy the emptyTranslationModel.json to en-GB.json')
-executeCommand(
-  `cp ${translationDirectory}/translationTemplateWithContext.json ${translationDirectory}/en-GB.json`,
-)
+copyTemplateWithContextToEnGb()
 
-// clear all the other translation files by emptyTranslationModel to de-DE.json, fr-FR.json, zh-CN.json and en-US.json
-console.log('clear all the other translation files by emptyTranslationModel')
-executeCommand(
-  `cp ${translationDirectory}/translationTemplateWithoutContext.json ${translationDirectory}/de-DE.json`,
-)
-executeCommand(
-  `cp ${translationDirectory}/translationTemplateWithoutContext.json ${translationDirectory}/fr-FR.json`,
-)
-executeCommand(
-  `cp ${translationDirectory}/translationTemplateWithoutContext.json ${translationDirectory}/zh-CN.json`,
-)
-executeCommand(
-  `cp ${translationDirectory}/translationTemplateWithoutContext.json ${translationDirectory}/en-US.json`,
-)
+// // clear all the other translation files by emptyTranslationModel to de-DE.json, fr-FR.json, zh-CN.json and en-US.json
+// console.log('clear all the other translation files by emptyTranslationModel')
+clearAllOtherTranslationFilesWithEmptyFile({
+  translationFiles: otherLanguage,
+})
 
 // commit the changes
 console.log('commit the changes')
-executeCommand(`git add ${translationDirectory}/en-GB.json`)
+executeCommand(`git add ${baseDirectory}/en-GB.json`)
+executeCommand(`git add ${homeScreenDirectory}/en-GB.json`)
 
 executeCommand(`git commit -m "test: update en-GB.json"`)
 
 // copy the exampleTranslation.json to en-GB.json
-console.log('copy the exampleTranslation.json to en-GB.json')
-executeCommand(
-  `cp ${translationDirectory}/translationTemplateWithText.json ${translationDirectory}/en-GB.json`,
-)
+
+copyTemplateWithTextToEnGB()
 
 // stage the change
-console.log('stage the change')
-executeCommand(`git add ${translationDirectory}/en-GB.json`)
+
+stageEnGbFiles()
 
 // run the script
 console.log('run the script')
 updateTranslationFileOnCommit({
   openAiApiKey,
-  translationDirectory,
   defaultLanguage: 'en-GB',
   otherLanguage,
 })
