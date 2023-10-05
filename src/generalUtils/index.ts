4c9422b7ff85e8c6a75f6cd1b4054b9aa6b7fc29
Favot <70208301+Favot@users.noreply.github.com>
2023-10-05 12:20:43 +0200

    Rename utils folder into GeneralUtils

---
+++ src/generalUtils/index.ts
@@ -0,0 +1,5 @@
+export { getChangedTranslationFile } from '../translation/getChangedTranslationFile'
+export { processTranslatedFile } from '../translation/processTranslatedFile'
+export { generateOpenAiAssistantContent } from './generateOpenAiAssistantContent'
+export { generateOpenAiSystemContent } from './generateOpenAiSystemContent'
+export { updateOtherLanguage } from './updateOtherLanguage'
