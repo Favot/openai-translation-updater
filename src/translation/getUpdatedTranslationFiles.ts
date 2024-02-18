fde47c80508e5b21ca002ef9abf45ab287c3f8b8
Favot <70208301+Favot@users.noreply.github.com>
2024-02-18 11:01:30 +0100

    Refactor: Return type and export

--- src/translation/getUpdatedTranslationFiles.ts
+++ src/translation/getUpdatedTranslationFiles.ts
@@ -1,10 +1,10 @@
-import { getTranslationFiles } from './getTranslationFiles';
+import { getTranslationFiles } from './getTranslationFiles'
 
 export const getUpdatedTranslationFiles = async ({
   defaultLanguage,
 }: {
   defaultLanguage: string;
-}) => {
+}) : Promise<string[] | string>=> {
   const command = `git diff --name-only | grep ${defaultLanguage}.json`;
 
   const updatedTranslationFiles = getTranslationFiles({ command });
